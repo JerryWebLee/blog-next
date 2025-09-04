0fea72fcc45d2af1d422aeda1cde957ae8bf881e
JerryWebLee <jerryweblee@outlook.com>
2025-09-05 00:26:22 +0800

    feat: 删除关于定价和测试页面的组件，简化项目结构，优化代码维护性

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,9 @@
-import { Counter } from "@/components/counter";
 import { title } from "@/components/primitives";
 
 export default function AboutPage() {
   return (
     <div>
       <h1 className={title()}>About</h1>
-      <Counter />
     </div>
   );
 }
