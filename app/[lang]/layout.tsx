087788215942c263e17961f5982f27e55ae51758
JerryWebLee <jerryweblee@outlook.com>
2025-09-19 23:17:18 +0800

    fix: 更新根布局组件以支持多语言和主题切换，简化代码结构，提升可维护性

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,22 +1,10 @@
-import type { Metadata } from "next";
-import { Inter, Urbanist } from "next/font/google";
-
 import "@/styles/globals.scss";
 
-import clsx from "clsx";
+import { Metadata } from "next";
 
 import { ConditionalLayout } from "@/components/layout/conditional-layout";
 import { getDictionary } from "@/lib/dictionaries";
 import { Locale } from "@/types";
-import { Providers } from "../providers";
-
-const inter = Inter({ subsets: ["latin"] });
-const urbanist = Urbanist({ subsets: ["latin"] });
-
-// 生成静态参数
-export async function generateStaticParams() {
-  return [{ lang: "zh-CN" }, { lang: "en-US" }, { lang: "ja-JP" }];
-}
 
 // 生成元数据
 export async function generateMetadata({ params }: { params: Promise<{ lang: Locale }> }): Promise<Metadata> {
@@ -36,27 +24,5 @@ export default async function RootLayout({
 }) {
   const { lang } = await params;
 
-  return (
-    <html lang={lang} suppressHydrationWarning>
-      <body
-        className={clsx(
-          urbanist.className
-          // "bg-radial from-[hsla(var(--heroui-background))] to-[hsla(var(--heroui-background-1))]"
-        )}
-      >
-        <Providers
-          themeProps={{
-            defaultTheme: "dark",
-            attribute: "class",
-            value: {
-              dark: "dark",
-              light: "light",
-            },
-          }}
-        >
-          <ConditionalLayout lang={lang}>{children}</ConditionalLayout>
-        </Providers>
-      </body>
-    </html>
-  );
+  return <ConditionalLayout lang={lang}>{children}</ConditionalLayout>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,32 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+import { Inter, Urbanist } from "next/font/google";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+
+const inter = Inter({ subsets: ["latin"] });
+const urbanist = Urbanist({ subsets: ["latin"] });
+// 生成静态参数
+export async function generateStaticParams() {
+  return [{ lang: "zh-CN" }, { lang: "en-US" }, { lang: "ja-JP" }];
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html suppressHydrationWarning>
+      <body className={clsx(inter.className)}>
+        <Providers
+          themeProps={{
+            defaultTheme: "dark",
+            attribute: "class",
+            value: {
+              dark: "dark",
+              light: "light",
+            },
+          }}
+        >
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
 }
